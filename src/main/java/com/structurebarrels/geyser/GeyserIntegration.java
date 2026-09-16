dfb912a7be4835c154b8a27d1d1b2a5a0fde5383
BomBad <194823451+BomBad8@users.noreply.github.com>
2026-09-17 00:03:59 +0100

    h

--- src/main/java/com/structurebarrels/geyser/GeyserIntegration.java
+++ src/main/java/com/structurebarrels/geyser/GeyserIntegration.java
@@ -3,7 +3,7 @@ package com.structurebarrels.geyser;
 import com.structurebarrels.StructureBarrels;
 import com.structurebarrels.item.StructureBarrelItem;
 import net.minecraft.core.registries.BuiltInRegistries;
-import org.geysermc.event.Subscribe;
+import org.geysermc.event.subscribe.Subscribe;
 import org.geysermc.geyser.api.event.lifecycle.GeyserDefineCustomItemsEvent;
 import org.geysermc.geyser.api.item.custom.v2.NonVanillaCustomItemDefinition;
 import org.geysermc.geyser.api.util.Identifier;
